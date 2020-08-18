6e5d83446a5ff84ba846f740e7b7779d7b584897
architjain-12 <architjain08@gmail.com>
2020-08-18 10:42:47 +0530

    Updates

---
+++ precache-manifest.b185c1eed6cde8b3e020236c734a2c59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1ca08dd642cf1d05f41c0090fa29d2",
+    "url": "/react-cards/index.html"
+  },
+  {
+    "revision": "6d0a1f4c03f89563cb15",
+    "url": "/react-cards/static/css/main.1945acb6.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/react-cards/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cards/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0a1f4c03f89563cb15",
+    "url": "/react-cards/static/js/main.966a5252.chunk.js"
+  },
+  {
+    "revision": "7a980d1eff367423dbb2",
+    "url": "/react-cards/static/js/runtime-main.9ad47492.js"
+  }
+]);
